89609686869097d676b8ef60a3e8efa56874c01c
AnnaTarnavska <anja.t0410@gmail.com>
2025-03-28 21:30:52 +0200

    create

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import "modern-normalize";
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Sip Happens Café</h1>
+        <p>Please leave your feedback about our service by selecting one of the options below.</p>
+      </div>
+      {/* <Feedback />
+      <Options /> */}
+    </>
+  )
+}
+
+export default App
